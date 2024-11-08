a76b4c5ad963d2550bad4e71f0d814e36bf61f79
sssahib <sssahib@crimson.ua.edu>
2024-11-08 14:29:12 -0600

    Finished chap 11

--- lox/src/resolver.rs
+++ lox/src/resolver.rs
@@ -1,3 +1,4 @@
+use std::cell::Ref;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::ops::Deref;
@@ -13,10 +14,20 @@ pub struct Resolver<'a> {
     interpreter: &'a Interpreter,
     scopes: RefCell<Vec<RefCell<HashMap<String, bool>>>>,
     had_error: RefCell<bool>,
+    current_function: RefCell<FunctionType>,
+}
+
+#[derive(PartialEq)]
+enum FunctionType {
+    None,
+    Function
 }
 
 impl<'a> StmtVisitor<()> for Resolver<'a> {
     fn visit_return_stmt(&self, _: Rc<Stmt>, stmt: &ReturnStmt) -> Result<(), LoxResult> {
+        if *self.current_function.borrow() == FunctionType::None {
+            self.error(&stmt.keyword, "Can't return from top-level code");
+        }
         if let Some(value) = stmt.value.clone() {
             self.resolve_expr(value)?;
         }
@@ -26,7 +37,7 @@ impl<'a> StmtVisitor<()> for Resolver<'a> {
         self.declare(&stmt.name);
         self.define(&stmt.name);
 
-        self.resolve_function(stmt)?;
+        self.resolve_function(stmt, FunctionType::Function)?;
 
         Ok(())
     }
@@ -140,6 +151,7 @@ impl<'a> Resolver<'a> {
             interpreter,
             scopes: RefCell::new(Vec::new()),
             had_error: RefCell::new(false),
+            current_function: RefCell::new(FunctionType::None),
         }
     }
 
@@ -194,7 +206,8 @@ impl<'a> Resolver<'a> {
         }
     }
 
-    fn resolve_function(&self, function: &FunctionStmt) -> Result<(), LoxResult> {
+    fn resolve_function(&self, function: &FunctionStmt, ftype: FunctionType) -> Result<(), LoxResult> {
+        let enclosing_function = self.current_function.replace(ftype);
         self.begin_scope();
 
         for param in function.params.iter() {
@@ -205,8 +218,10 @@ impl<'a> Resolver<'a> {
         self.resolve(&function.body)?;
 
         self.end_scope();
+        self.current_function.replace(enclosing_function);
         Ok(())
     }
+
     fn error(&self, token: &Token, message: &str) {
         self.had_error.replace(true);
         LoxResult::runtime_error(token, message);
